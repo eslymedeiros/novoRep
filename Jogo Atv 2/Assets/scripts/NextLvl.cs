2bbbd7b0087a538662df80ebec6bc62539190f5a
xuoxy <145376591+eslymedeiros@users.noreply.github.com>
2024-08-30 20:03:48 -0300

    jogo completo

---
+++ Jogo Atv 2/Assets/scripts/NextLvl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLvl : MonoBehaviour
+{
+    public string next;
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Player"){
+            SceneManager.LoadScene(next);
+        }
+    }
+}
